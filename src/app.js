d124d4a00d73c67ddabb8ed73cdecede81ed47d1
sarnava1 <konarsarnava@gmail.com>
2019-01-13 00:22:19 +0530

    commit 5: installed react and react-dom

--- src/app.js
+++ src/app.js
@@ -1,6 +1,2 @@
-import isSenior, { isAdult, canDrink } from './person.js';
-
-console.log(isAdult(20));
-console.log(canDrink(18));
-console.log(isSenior(65));
-
+import React from 'react';
+import ReactDOM from 'react-dom';
---
+++ src/playground/import_export.js
@@ -0,0 +1,6 @@
+import isSenior, { isAdult, canDrink } from './person.js';
+
+console.log(isAdult(20));
+console.log(canDrink(18));
+console.log(isSenior(65));
+
