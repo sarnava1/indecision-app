d249a940070a3d898fdea569ec5bd38499a3572e
sarnava1 <konarsarnava@gmail.com>
2019-01-12 13:05:13 +0530

    commit 4: learnt about default import and export

--- public/bundle.js
+++ public/bundle.js
@@ -72,8 +72,9 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__person_js__ = __webpack_require__(1);
 
 
-console.log(__WEBPACK_IMPORTED_MODULE_0__person_js__["b" /* isAdult */](20));
+console.log(__WEBPACK_IMPORTED_MODULE_0__person_js__["c" /* isAdult */](20));
 console.log(__WEBPACK_IMPORTED_MODULE_0__person_js__["a" /* canDrink */](18));
+console.log(__WEBPACK_IMPORTED_MODULE_0__person_js__["b" /* default */](65));
 
 
 
@@ -82,11 +83,12 @@ console.log(__WEBPACK_IMPORTED_MODULE_0__person_js__["a" /* canDrink */](18));
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "b", function() { return isAdult; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "c", function() { return isAdult; });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return canDrink; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "b", function() { return isSenior; });
 const isAdult = (age) => age>=18;
 const canDrink = (age) => age>=21;
-
+const isSenior= (age) => age>=65;
 
 
 
--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
-import { isAdult, canDrink } from './person.js';
+import isSenior, { isAdult, canDrink } from './person.js';
 
 console.log(isAdult(20));
 console.log(canDrink(18));
+console.log(isSenior(65));
 
