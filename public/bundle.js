a9137028671939e798f83bc7a2723e24419bd0f0
sarnava1 <konarsarnava@gmail.com>
2019-01-12 11:58:01 +0530

    commit 3: learnt about import and export

--- public/bundle.js
+++ public/bundle.js
@@ -65,7 +65,29 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports) {
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__person_js__ = __webpack_require__(1);
+
+
+console.log(__WEBPACK_IMPORTED_MODULE_0__person_js__["b" /* isAdult */](20));
+console.log(__WEBPACK_IMPORTED_MODULE_0__person_js__["a" /* canDrink */](18));
+
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "b", function() { return isAdult; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return canDrink; });
+const isAdult = (age) => age>=18;
+const canDrink = (age) => age>=21;
+
+
 
 
 
---
+++ src/app.js
@@ -0,0 +1,5 @@
+import { isAdult, canDrink } from './person.js';
+
+console.log(isAdult(20));
+console.log(canDrink(18));
+
